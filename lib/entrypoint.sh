fc7682bbcfd93f1812ce63f4e93fade33e0313c9
Johan de Jager <johan@laptify.nl>
2019-10-14 22:05:44 +0200

    Fixed entrypoint

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 
 if [ -z "$PROJECT_DIRECTORY" ]
 then
-  cd $PROJECT_DIRECTORY
+  PROJECT_DIRECTORY="."
 fi
 
+cd $PROJECT_DIRECTORY
 npm install
 
 NODE_PATH=node_modules node /action/lib/run.js
