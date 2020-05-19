d01117d81b611a26ea39c9f7a4ac1d87ed06f30e
Hal Lee <hal@lee.me>
2020-05-19 12:27:37 -0400

    Self-linting action (#6)

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -8,5 +8,3 @@ cd "${2:-.}" || echo "source root not found"
 [ -f package-lock.json ] && npm install
 
 NODE_PATH=node_modules GITHUB_TOKEN=$1 SOURCE_ROOT=${2:-.} node /action/lib/run.js
-
-rm -rf node_modules # cleanup to prevent some wired permission errors later on
