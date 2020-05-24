627b0c9c87936606be93da4404381c85896ee7b3
Michael <MichaelSp@users.noreply.github.com>
2020-05-24 16:07:55 +0200

    Cleanup node_modules (#18)

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -8,3 +8,5 @@ cd "${2:-.}" || echo "source root not found"
 [ -f package-lock.json ] && npm install
 
 NODE_PATH=node_modules GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
+
+rm -rf node_modules # cleanup to prevent some weird permission errors later on 
